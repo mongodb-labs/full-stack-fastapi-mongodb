9402d5818b79544586df234fe2ee08cc04c3e161
Jibola <jib.adegunloye@mongodb.com>
2023-11-02 10:13:02 -0400

    return backend CORS

--- {{cookiecutter.project_slug}}/backend/app/app/main.py
+++ {{cookiecutter.project_slug}}/backend/app/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
