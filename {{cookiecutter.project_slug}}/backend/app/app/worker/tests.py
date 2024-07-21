92b90d144d4ba57e282ec3d0a17f914eeccb8c56
scott charlesworth <scott@kenja.io>
2024-07-21 15:31:04 +0200

    # Raven changed to Sentry SDK https://docs.sentry.io/platforms/python/migration/raven-to-sentry-sdk/

--- {{cookiecutter.project_slug}}/backend/app/app/worker/tests.py
+++ {{cookiecutter.project_slug}}/backend/app/app/worker/tests.py
@@ -1,10 +1,19 @@
-from raven import Client
+import sentry_sdk
 import asyncio
 
 from app.core.celery_app import celery_app
 from app.core.config import settings
 
-client_sentry = Client(settings.SENTRY_DSN)
+client_sentry = sentry_sdk.init(
+    dsn=settings.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 
 @celery_app.task(acks_late=True)
